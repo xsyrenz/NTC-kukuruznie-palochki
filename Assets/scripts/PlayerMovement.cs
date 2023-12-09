c4ed254e3f272ab60a0518336469b619ca6dce9d
Ekzo-sk <123snusmumrik321@gmail.com>
2023-12-09 13:21:23 +0300

    Created turrel with arrows

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,52 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float hp;
+    public float speed = 3f;
+    public float jumpForce = 15f;
 
-    public Vector2 moveVector; // Вектор, возвращающий изменения персонажа по оси x и y для передачи в Rigidbody и изменения положения игрока.
-    public float speed = 2f; // Переменная, отвечающая за скорость передвижения персонажа.
-    public float jumpForce = 7f; // Переменная, отвечающая за силу с которой персонаж отталкивается от земли. Фиксированное значение пикселей вверх.
+    private Rigidbody2D rb;
+
+    public LayerMask Ground;
+    public LayerMask Honey;
+
+    public bool onGround;
+    public bool isSlowed;
+
+    [Range(-10f, 5f)] public float CheckGroundOffsetY = 0f;
+    [Range(0, 5f)] public float CheckGroundRadius = 0.1f;
+
+    [Range(-10f, 5f)] private float isSlowedOffsetY = 0f;
+    [Range(0, 5f)] private float isSlowedRadius = 0.1f;
+
     public bool djump = false;
-    public bool dj = true;
+    public float djumpout = 0f;
+    public bool dj = false;
 
-    public bool onGround; // Проверка на касание земли.
-    public LayerMask Ground; // Функциональный слой, от которого отталкивается персонаж.
-    [Range(-5f, 5f)] public float CheckGroundOffsetY = 0f; // Переменная смещения коллайдера по y.
-    [Range(0, 5f)] public float CheckGroundRadius = 0.1f; // Переменная радиуса коллайдера.
-    public Rigidbody2D rb; // Создание компонента.
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // Получение компонента Rigidbody для дальнейщего взаимодеиствия через него с физикой (движение персонажа).
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Move();
-    }
+        djumpOut();
 
-    void Move()
-    {
-        moveVector.x = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+        onGround = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y + CheckGroundOffsetY), CheckGroundRadius, Ground);
+        isSlowed = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y + isSlowedOffsetY), isSlowedRadius, Honey);
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
 
-        onGround = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y + CheckGroundOffsetY), CheckGroundRadius, Ground); // Проверка входит ли в созданный коллайдер, коллайдер с функциональным слоем Земля.
-
-        if (Input.GetKeyDown(KeyCode.Space) && onGround) // Проверка, может ли персонаж прыгать.
+        if (Input.GetKeyDown(KeyCode.Space)) // Проверка, может ли персонаж прыгать.
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Задание скорости движения по y.
-        }    
+            if (onGround)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Задание скорости движения по y.
+                if (djump == true)
+                {
+                    dj = true;
+                }
+            }
+            else if (dj)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Задание скорости движения по y.
+                dj = false;
+            }
+        }
+
+        if (isSlowed)
+        {
+            speed = 0.5f;
+            jumpForce = 3;
+        }
+        else
+        {
+            speed = 10f;
+            jumpForce = 15f;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Comb"))
+        {
+            djump = true;
+            djumpout = 0f;
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void djumpOut()
     {
-        if (collision.gameObject.CompareTag("comb")) {
-            djump = true;
+        if (djump)
+        {
+            if (djumpout < 5f)
+            {
+                djumpout += Time.deltaTime;
+            }
+            else
+            {
+                djump = false;
+                djumpout = 0f;
+            }
         }
     }
 }
